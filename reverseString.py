c78ff8f7a58e0d1d57550eb0fb451c91ed5107e0
bme3412 <54747855+bme3412@users.noreply.github.com>
2023-09-06 17:29:26 -0400

    Add files via upload

---
+++ reverseString.py
@@ -0,0 +1,11 @@
+# Given a string, reverse all of its characters and return the resulting string
+string_1 = 'Cat'
+string_2 = 'The Daily Byte'
+string_3 = 'civic'
+
+def string_reverse(string):
+    print(string[::-1])
+
+x = string_reverse(string_1)
+y = string_reverse(string_2)
+z = string_reverse(string_3)
